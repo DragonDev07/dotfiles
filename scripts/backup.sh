b3fe7acdb585209930c2216d5196919d3e9c10bb
Teo Welton <teowelton@gmail.com>
2024-08-07 17:38:38 +0300

    Setup Anyrun, update Hyprland Config, and Some script changes

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,7 +1,10 @@
-echo "---------------------- BACKUP DOTFILES ----------------------"
-echo "Please confirm that this script will backup dotfiles and overwrite any changes in this git repo (y/N):"
 
+echo "---------------------- BACKUP DOTFILES ----------------------"
+echo "Please confirm that this script will backup dotfiles and overwrite any changes in this git repo (y/N): "
 # TODO: Check if user given input is "y" or "Y", if not, exit program
+
+echo "Please select current OS (1: MacOS, 2: Linux): "
+# TODO: Check if user given input is "1" or "2" and backup dotfiles accordingly
 
 echo "-------------------------------------------------------------"
 
